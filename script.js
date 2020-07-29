45752c9c2e8f6ac3a57154bd0ae1e53185862108
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-07-28 22:04:24 -0400

    firstHtml
    front end stuff

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// let userName = prompt("Hi, what is you name?");
+
+// if (userName === "")
+//     {}
+// else if (userName)
+//     alert("Nice to meet you " + userName);
+// else{}
+
+
+// let userGender = window.prompt("What is your Gender?");
+
+// alert(userName + " is a " + userGender);
+
+// let question = prompt("Donde nacio Jesus?");
+// if (question === "belen" || question === "Belen")
+//     alert("Correcto!");
+
+let mathQues = prompt("What's '9 + 10?");
+
