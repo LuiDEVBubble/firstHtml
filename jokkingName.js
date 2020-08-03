d727d00e3a3cef39ba46f9cadd5e224bb91cf000
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-08-03 12:02:25 -0400

    JK

---
+++ jokkingName.js
@@ -0,0 +1,9 @@
+let userName = prompt("What is your name?");
+
+
+let currentAge = prompt("What is your age?");
+let age = parseInt(currentAge);
+
+alert("Hi " + userName + " I didn't know you were " + (age + 40) + " years old!");
+alert("Just kidding! I know you're " + age);
+
