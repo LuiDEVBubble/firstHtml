91a86b998462880e00e3f3f9becbd96a3121a33f
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-08-03 11:59:19 -0400

    asking the user for name and age

---
+++ stringToInt.js
@@ -0,0 +1,8 @@
+let userName = promt("What is your name?");
+
+
+let currentAge = prompt("What is your age?");
+let age = parseInt(currentAge);
+
+alert("Hi " + userName + " I didn't know you were " + age + " years old!");
+
