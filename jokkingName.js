40cc72734d69b871e432f09ee22da0baa42b949b
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-08-03 12:08:42 -0400

    Message

--- jokkingName.js
+++ jokkingName.js
@@ -2,7 +2,7 @@ let userName = prompt("What is your name?");
 
 
 let currentAge = prompt("What is your age?");
-let age = parseInt(currentAge);
+let age = parseInt(currentAge); // parseInt is the function that is used to convert strings into integers
 
 alert("Hi " + userName + " I didn't know you were " + (age + 40) + " years old!");
 alert("Just kidding! I know you're " + age);
